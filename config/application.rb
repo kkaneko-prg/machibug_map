4900fb00cdd1ba9f65f1de2ec1675af11e4a1147
金子夏純 <kanekokasumi@kkaneko.local>
2021-12-10 21:39:29 +0900

    Add: ファイル生成の設定

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module MachibugMap
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
   end
 end
