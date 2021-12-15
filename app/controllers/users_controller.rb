16ad6981082e435314be08f260d2b33d80e5c967
金子夏純 <kanekokasumi@kkaneko.local>
2021-12-15 12:37:50 +0900

    Fix: users_controllerとviewの一部修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,15 +15,10 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to login_path
+    else
+      render :new
     end
   end
 
