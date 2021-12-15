729cb9f96a7f6e94ae561b7f898ce96e1f4b57be
金子夏純 <kanekokasumi@kkaneko.local>
2021-12-15 12:57:16 +0900

    Fix: 各controllerの一部修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  skip_before_action :require_login, only: %i[index new create]
 
   def index
     @users = User.all
