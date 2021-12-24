7392a28c0bc753003cc923f6b95796762ad7be6c
金子夏純 <kanekokasumi@kkaneko.local>
2021-12-24 15:51:39 +0900

    Fix: アソシエーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  has_many :users 
+  has_many :posts 
 end
