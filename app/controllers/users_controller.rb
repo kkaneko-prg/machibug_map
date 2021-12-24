67fdfb4c3f4445d0604ef95e5255de5033a09ffa
金子夏純 <kanekokasumi@kkaneko.local>
2021-12-24 12:39:30 +0900

    Add: オートログインできるようにcreateメソッドを一部修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,16 +16,18 @@ class UsersController < ApplicationController
     end
   end
 
-  def edit; end
-
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to login_path
+      # ユーザー登録が成功したら、そのままログインが完了した状態になる。
+      auto_login(@user)
+      redirect_to root_url
     else
       render :new
     end
   end
+
+  def edit; end
 
   def update
     respond_to do |format|
