a107d674480340a9abb4052f84fb2d98f31d69a4
kaneko kasumi <kaneko.prog@gmail.com>
2022-01-07 21:26:59 +0900

    Add: spotsモデルを追加

---
+++ app/models/spot.rb
@@ -0,0 +1,3 @@
+class Spot < ApplicationRecord
+  belongs_to :post
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_125716) do
+ActiveRecord::Schema.define(version: 2022_01_07_121745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,16 @@ ActiveRecord::Schema.define(version: 2021_12_23_125716) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "spots", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_spots_on_post_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -66,4 +76,5 @@ ActiveRecord::Schema.define(version: 2021_12_23_125716) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "spots", "posts"
 end
