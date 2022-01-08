5644fd341c82d685604540f3bb8ecd0af22a8397
kaneko kasumi <kaneko.prog@gmail.com>
2022-01-08 20:11:41 +0900

    Add: ストロングパラメーターにカラムを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:caption, photos: [])
+    params.require(:post).permit(:caption, :address, photos: [])
   end
 end
