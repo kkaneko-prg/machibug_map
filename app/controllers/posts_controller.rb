28dae5dd701c9e4fd6a4fcd2688d252853f63059
kaneko kasumi <kaneko.prog@gmail.com>
2022-01-15 12:04:22 +0900

    Add: showアクションにいいねに関する記述を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @comment = Comment.new
     @comments = @post.comments.includes(:user).order(created_at: :desc)
+    @like = Like.new
   end
 
   def new
