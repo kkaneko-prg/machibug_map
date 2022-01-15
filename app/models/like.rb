c0e9ea34c96939e7ee3b29ca100a6ad4ea46a73d
kaneko kasumi <kaneko.prog@gmail.com>
2022-01-15 11:58:45 +0900

    Add: バリデーションを追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
-  belongs_to :post
   belongs_to :user
+  belongs_to :post
+
+  # validates_uniqueness_of :post_id, scope: :user_id
+  # user_idとpost_idの組み合わせが1組しかないようにするバリデーション。
+  validates :user_id, uniqueness: { scope: :post_id }
 end
