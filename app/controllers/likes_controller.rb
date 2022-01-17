754d07a9e4e87b0dd3fe42ea374d1f22a6382aa1
kaneko kasumi <kaneko.prog@gmail.com>
2022-01-17 20:53:52 +0900

    Fix: シングルクォーテーションに修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -7,7 +7,7 @@ class LikesController < ApplicationController
       current_user.likes.create(post_id: @post.id)
     else
       redirect_back fallback_location: root_path
-      flash[:alert] = "ログインが必要です。"
+      flash[:alert] = 'ログインが必要です。'
     end
   end
 
