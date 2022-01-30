e28e208762b7330a1918eb56b5201d186b13fb8b
kaneko kasumi <kaneko.prog@gmail.com>
2022-01-30 16:33:06 +0900

    Add: リダイレクトの際のlogin_pathにmain_appのプレフィックスを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
+  # デフォルトでrequire_loginをアクションの前に行う設定
   before_action :require_login
+  # 使用可能なフラッシュメッセージの種類を追加
   add_flash_types :success, :info, :warning, :danger
 
   private
 
+  # 未ログインユーザーがアクセスした際にrequire_loginで弾かれた場合の処理
   def not_authenticated
-    redirect_to login_path, alert: 'ログインが必要です'
+    redirect_to main_app.login_path, alert: 'ログインが必要です'
   end
 end
