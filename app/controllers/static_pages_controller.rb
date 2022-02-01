cd5b93b43e9e5b6af3f1ee2712562da84154d9f9
kaneko kasumi <kaneko.prog@gmail.com>
2022-02-01 21:34:16 +0900

    Add: static_pages_controllerのアクションを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,14 @@
 class StaticPagesController < ApplicationController
-  skip_before_action :require_login, only: %i[top]
+  skip_before_action :require_login, only: %i[about howto terms privacy]
+  # とは？
+  def about; end
 
-  def top; end
+  # 使い方
+  def howto; end
+
+  # ご利用規約
+  def terms; end
+
+  # プライバシーポリシー
+  def privacy; end
 end
