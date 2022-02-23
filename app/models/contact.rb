076f3f7260732bdb1b1bab510585b7f7b1145867
kaneko kasumi <kaneko.prog@gmail.com>
2022-02-23 22:20:44 +0900

    Fix: 入力ミスを修正

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,5 @@
 class Contact < ApplicationRecord
   validates :name,    presence: true
   validates :email,   presence: true
-  validates :content, presence: true
+  validates :message, presence: true
 end
